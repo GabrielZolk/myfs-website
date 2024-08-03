a6fe93ba6871cdc1f108515297ccd659db28548b
Gabriel Zolk <dialixkat@gmail.com>
2024-08-03 02:10:28 -0300

    new sections

--- src/App.vue
+++ src/App.vue
@@ -65,10 +65,52 @@
     <div ref="about" class="about">
       <div class="about-image-container">
         <img src="./assets/aboutbg.png" width="100%" alt="about" class="about-bg-image">
-        <img src="./assets/aboutshepng.png" class="about-image-she-photo" alt="about">
+        <img src="./assets/my.png" class="about-image-she-photo" alt="about">
+      </div>
+      <div class="about-title-container">
+        <span class="about-title">
+          Quem sou
+        </span>
+      </div>
+      <div class="hifen"></div>
+      <div>
+        <p class="about-text">
+          Sou a mamãe do Bruce, uma pessoa incrível e a melhor fotógrafa do mundo!!!!!!
+        </p>
       </div>
     </div>
-    
+    <div v-if="expandedImage" class="expanded-image-overlay" @click="closeExpandedImage">
+      <img :src="expandedImage" class="expanded-image" />
+      <button class="close-button" @click="closeExpandedImage">X</button>
+    </div>
+    <div ref="portfolio" class="portfolio-container">
+      <div class="portfolio-image-first-container">
+        <img src="./assets/portfolio/port1.jpg" class="portfolio-image" alt="portfolio" @click="expandImage(require('@/assets/portfolio/port1.jpg'))">
+        <img src="./assets/portfolio/port2.jpg" class="portfolio-image" alt="portfolio" @click="expandImage(require('@/assets/portfolio/port2.jpg'))">
+        <img src="./assets/portfolio/port3.jpg" class="portfolio-image" alt="portfolio" @click="expandImage(require('@/assets/portfolio/port3.jpg'))">
+        <img src="./assets/portfolio/port4.jpg" class="portfolio-image" alt="portfolio" @click="expandImage(require('@/assets/portfolio/port4.jpg'))">
+      </div>
+      <div class="portfolio-image-second-container">
+        <img src="./assets/portfolio/port5.jpg" class="portfolio-image" alt="portfolio" @click="expandImage(require('@/assets/portfolio/port5.jpg'))">
+        <img src="./assets/portfolio/port6.jpg" class="portfolio-image" alt="portfolio" @click="expandImage(require('@/assets/portfolio/port6.jpg'))">
+        <img src="./assets/portfolio/port7.jpg" class="portfolio-image" alt="portfolio" @click="expandImage(require('@/assets/portfolio/port7.jpg'))">
+        <img src="./assets/portfolio/port8.jpg" class="portfolio-image" alt="portfolio" @click="expandImage(require('@/assets/portfolio/port8.jpg'))">
+      </div>
+      <div class="portfolio-image-third-container">
+        <img src="./assets/portfolio/port9.jpg" class="portfolio-image" alt="portfolio" @click="expandImage(require('@/assets/portfolio/port9.jpg'))">
+        <img src="./assets/portfolio/port10.jpg" class="portfolio-image" alt="portfolio" @click="expandImage(require('@/assets/portfolio/port10.jpg'))">
+        <img src="./assets/portfolio/port11.jpg" class="portfolio-image" alt="portfolio" @click="expandImage(require('@/assets/portfolio/port11.jpg'))">
+        <img src="./assets/portfolio/port12.jpg" class="portfolio-image" alt="portfolio" @click="expandImage(require('@/assets/portfolio/port12.jpg'))">
+      </div>
+      <div class="portfolio-image-fourth-container">
+        <img src="./assets/portfolio/port13.jpg" class="portfolio-final-image" alt="portfolio">
+      </div>
+      <div class="portfolio-image-fourth-container-text">
+        <span class="portfolio-final-image-text">
+          "A Fotografia é uma viagem as suas melhores recordações. 📸" 
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -85,11 +127,19 @@ export default {
   data() {
     return {
       isActive: false,
+      expandedImage: null,
     };
   },
   methods: {
     toggleMenu() {
       this.isActive = !this.isActive;
+    },
+
+    expandImage(imageSrc) {
+      this.expandedImage = imageSrc;
+    },
+    closeExpandedImage() {
+      this.expandedImage = null;
     },
   },
 }
@@ -215,7 +265,7 @@ export default {
   font-family: 'Inria Serif', sans-serif;
   font-size: 40px;
   position: absolute;
-  top: 50%;
+  top: 25%;
   left: 50%;
   transform: translate(-50%, -50%);
   width: 100%;
@@ -228,7 +278,7 @@ export default {
 
 .about {
   position: relative;
-  margin: 10px 10px 10px 0px;
+  margin: 10px 0px 10px 10px;
 }
 
 .about-image-container {
@@ -246,4 +296,96 @@ export default {
   max-width: 200px;
 }
 
+.hifen {
+  width: 10%;
+  height: 1px;
+  background-color: #333;
+  margin-bottom: 20px
+}
+
+.about-title {
+  font-family: 'Inria Serif', sans-serif;
+  font-size: 30px;
+}
+
+.about-title-container {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.portfolio-image {
+  width: 25vw;
+  height: 20vw;
+  object-fit: cover;
+  display: flex;
+}
+
+.portfolio-image-first-container {
+  display: flex;
+}
+
+.portfolio-image-second-container {
+  display: flex;
+}
+
+.portfolio-image-third-container {
+  display: flex;
+}
+
+.portfolio-final-image {
+  width: 100vw;
+  height: 60vw;
+  object-fit: cover;
+  display: flex;
+  opacity: 0.4;
+}
+
+.portfolio-image-fourth-container-text {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.portfolio-final-image-text {
+  font-family: 'Inria Serif', sans-serif;
+  font-size: 30px;
+  position: absolute;
+  bottom: 0;
+  color: black;
+  padding: 20px;
+  font-weight: 800;
+}
+
+.expanded-image-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+}
+
+.expanded-image {
+  max-width: 90%;
+  max-height: 90%;
+  object-fit: contain;
+}
+
+.close-button {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  background: transparent;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+  font-size: 24px;
+  color: white;
+  font-weight: bold;
+}
 </style>
